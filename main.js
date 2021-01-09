02fd9e5032977561ab593c38b40520aef13508cc
Abdallah <abdallahchaar90@gmail.comh>
2021-01-09 14:36:09 +0100

    JavaScript Array alternativ

---
+++ main.js
@@ -0,0 +1,23 @@
+/* global console, alert, prompt*/
+
+var friendOne = "Hasan",
+
+    frindeTwo = "Soha",
+
+    frindeThree = "Ahmad",
+
+    frindeFour = "Zahra",
+
+    frindeFive = "Hind";
+
+var frinds = new Array();
+
+frinds[0] = "Hassan";
+
+frinds[1] = "Soha";
+
+frinds[2] = "Ahmad";
+
+frinds[3] = "Zahra";
+
+frinds[4] = "Hind";
